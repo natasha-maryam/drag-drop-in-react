6cdac77ab4b31b5ae592f5536a1b944e764a62a3
prodevXpert <prodevxpert@gmail.com>
2023-10-24 18:02:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-// import './App.css';
 
 const tasksList = [
   { id: "1", item: "Learn Japanese" },
