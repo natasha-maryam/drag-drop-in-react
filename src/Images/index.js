05f29ba72375fffda3bbda21314fb17cc3f7c749
Natasha <natasha.maryam2003@gmail.com>
2023-10-24 17:14:33 +0500

    creating drag drop between multiple columns

---
+++ src/Images/index.js
@@ -0,0 +1,5 @@
+export {default as Cato} from './cato.png'
+export {default as Gray} from './gary.png'
+export {default as KVN} from './kvn.png'
+export { default as Mooncake } from "./mooncake.png";
+export { default as Quinn } from "./quinn.png";
